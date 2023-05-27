7dd172aa112dc80e3d95791baa61c4413b6a7c75
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-05-28 02:10:12 +0900

    test

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # board-template
 
 template for simple web service
+
+## test command
+
+`curl -X POST --data-urlencode 'email=hoge@email.com' http://192.168.0.4:9999/create`
--- cmd/board/createHandler.go
+++ cmd/board/createHandler.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	// "github.com/jinzhu/gorm"
+	"github.com/shimiwaka/board-template/schema"
+	"github.com/shimiwaka/board-template/connection"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "CREATE!")
+	fmt.Fprintln(w, chi.URLParam(r, "email"))
+
+	db := connection.connectDB()
+
+	board := schema.Board{Owner: chi.URLParam(r, "email")}
+	db.Create(&board)
+
+	db.Close()
 }
