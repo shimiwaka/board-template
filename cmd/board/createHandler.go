3f53a12a14b77da23adc93ecef402ff5aee56899
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-05-28 02:17:57 +0900

    test

--- cmd/board/createHandler.go
+++ cmd/board/createHandler.go
@@ -7,7 +7,7 @@ import (
 	"github.com/go-chi/chi"
 	// "github.com/jinzhu/gorm"
 	"github.com/shimiwaka/board-template/schema"
-	"github.com/shimiwaka/board-template/connection"
+	"github.com/shimiwaka/board-template/connector"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
@@ -15,10 +15,12 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "CREATE!")
 	fmt.Fprintln(w, chi.URLParam(r, "email"))
 
-	db := connection.connectDB()
+	db := connector.connectDB()
 
 	board := schema.Board{Owner: chi.URLParam(r, "email")}
 	db.Create(&board)
+
+	schema.test()
 
 	db.Close()
 }
